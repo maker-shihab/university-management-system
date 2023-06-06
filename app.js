bea308063bef804dd7510cf6ac22f055e3e15a0c
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-06 22:18:18 +0600

    Modify for checking

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const maker = {
-    name: 'Maker',
-    age: 24,
-    role: 'Developer'
-}
-const maker2 = ['Maker', 24, 'Developer'];
-const funMaker = ([name, age, role]) => {
-    console.log(name);
-}
-funMaker(maker2);
 
