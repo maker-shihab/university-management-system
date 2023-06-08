cebe0fe73c5152c4af5d0ebe03a02fba84c53063
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-08 14:23:52 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-const port = 5000;
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // Parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully')
+})
 
-export default app;
+export default app
