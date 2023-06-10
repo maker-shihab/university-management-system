babc434a0a03a39420bbddef4c79f059ed179a36
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-10 10:31:20 +0600

    [Done] create user and create a router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import usersRouter from './app/modules/users/user.router'
 const app: Application = express()
 
 app.use(cors())
@@ -8,8 +9,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working successfully')
+// Application routes
+app.use('/api/v1/users/', usersRouter)
+
+// Testing
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
 })
 
 export default app
