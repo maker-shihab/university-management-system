aa410e8bd09ca6293106be405dc708b85d0c176d
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-13 01:35:07 +0600

    Global eroro and custom error

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import usersRouter from './app/modules/users/user.router'
 const app: Application = express()
 
@@ -12,9 +12,34 @@ app.use(express.urlencoded({ extended: true }))
 // Application routes
 app.use('/api/v1/users/', usersRouter)
 
+// Custome Error
+class ApiError extends Error {
+  statusCode: number
+
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message)
+    this.statusCode = statusCode
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
 // Testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully')
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  // throw new ApiError(400, 'Good boy this is a hand made error!')
+  next('Good boy this is error')
+})
+
+// global Error handler
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({ error: err })
+  } else {
+    res.status(500).json({ error: 'Something went wrong' })
+  }
 })
 
 export default app
