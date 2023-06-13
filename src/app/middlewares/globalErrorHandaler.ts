b6b6aa3593c11a58567c2da13683ce344831d7c3
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-13 23:57:24 +0600

    [DONE] Error handaler

--- src/app/middlewares/globalErrorHandaler.ts
+++ src/app/middlewares/globalErrorHandaler.ts
@@ -1,12 +1,49 @@
-import { NextFunction, Request, Response } from 'express'
+import { ErrorRequestHandler } from 'express'
+import config from '../../config'
+import ApiError from '../../error/ApiError'
+import handleValidationError from '../../error/handaleValidationError'
+import { IGenericErrorMessage } from '../../interfaces/error'
 
-const globalErrorHandaler = async (
-  err,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.status(400).json({ SokhinaError: err })
+const globalErrorHandaler: ErrorRequestHandler = (error, req, res, next) => {
+  let statusCode = 500
+  let message = 'Something went wrong!'
+  let errorMessages: IGenericErrorMessage[] = []
+
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error.message
+    errorMessages = error.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? error?.stack : undefined,
+  })
+
   next()
 }
 export default globalErrorHandaler
