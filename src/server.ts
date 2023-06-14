d3a144676eac6409da9910fc60a350a95c75edeb
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-14 12:05:45 +0600

    [13.3] Update

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,44 @@
+import { Server } from 'http'
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
 import { errorLogger, logger } from './shared/logger'
+
+process.on('uncaughtException', error => {
+  errorLogger.error(error)
+  process.exit(1)
+})
+
+let server: Server
+
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
     logger.info('🥌 Database connected successfully')
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`UMP listen on port ${config.port}`)
     })
   } catch (error) {
     errorLogger.error(`Failed to connect database ${error}`)
   }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 main()
+
+process.on('SIGABRT', () => {
+  logger.info('SIGTERM is Received')
+  if (server) {
+    server.close()
+  }
+})
