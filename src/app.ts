f9c3bd4f70e73baff045d74499784c08a99b20c4
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-15 16:38:29 +0600

    [DONE] Academic Semester

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,28 @@
-import cors from 'cors'
-import express, { Application } from 'express'
-import globalErrorHandaler from './app/middlewares/globalErrorHandaler'
-import usersRouter from './app/modules/user/user.router'
-const app: Application = express()
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandaler from './app/middlewares/globalErrorHandaler';
+import { AcademicSemesterRoute } from './app/modules/academicSemester/createAcademicSemester.route';
+import usersRouter from './app/modules/user/user.router';
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 
 // Parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Application routes
-app.use('/api/v1/users/', usersRouter)
+app.use('/api/v1/users/', usersRouter);
 
 // Testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
 //   throw new Error('Good boy this is a hand made error!')
 // })
 
-// global Error handler
-app.use(globalErrorHandaler)
+// Academic Semester
+app.use('/api/v1/academic-semesters', AcademicSemesterRoute);
 
-export default app
+// global Error handler
+app.use(globalErrorHandaler);
+
+export default app;
