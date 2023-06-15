4d86c01ebcffbbf98119496dd78355569750440a
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-15 12:40:53 +0600

    [Create] Academic Schema, Model, Interface, Route etc

---
+++ src/app/modules/academicSemester/academicSemester.constant.ts
@@ -0,0 +1,31 @@
+import {
+  IAcademicSemesterCodes,
+  IAcademicSemesterMonth,
+  IAcademicSemesterTitles,
+} from './academicSemester.interface';
+
+export const academicSemesteMonths: IAcademicSemesterMonth[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const academicSemesteTitles: IAcademicSemesterTitles[] = [
+  'Autumn',
+  'Summer',
+  'Fall',
+];
+export const academicSemesteCodes: IAcademicSemesterCodes[] = [
+  '01',
+  '02',
+  '03',
+];
---
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+import {
+  academicSemesteCodes,
+  academicSemesteMonths,
+  academicSemesteTitles,
+} from './academicSemester.constant';
+
+const createAcademicSemesterZodSchema = z.object({
+  body: z.object({
+    title: z.enum([...academicSemesteTitles] as [string, ...string[]], {
+      required_error: 'Title is required!',
+    }),
+    year: z.number({
+      required_error: 'Year is required!',
+    }),
+    code: z.enum([...academicSemesteCodes] as [string, ...string[]], {
+      required_error: 'Code is required!',
+    }),
+  }),
+  startMonth: z.enum([...academicSemesteMonths] as [string, ...string[]], {
+    required_error: 'Start month is required!',
+  }),
+  endMonth: z.enum([...academicSemesteMonths] as [string, ...string[]], {
+    required_error: 'End Month is required!',
+  }),
+});
+
+export const AcademicSemesterValidation = {
+  createAcademicSemesterZodSchema,
+};
